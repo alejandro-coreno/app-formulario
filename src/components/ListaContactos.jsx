b344ef0ec4931b24f7b01c9288f589858b61c0e8
Alejandro <alejandro@MacBook-Air-de-Alejandro.local>
2024-04-29 23:41:06 -0600

    Se agrega funcion de leer datos de firebase

--- src/components/ListaContactos.jsx
+++ src/components/ListaContactos.jsx
@@ -1,26 +1,41 @@
+import { useState, useEffect } from "react";
+import Contacto from "./Contacto";
 import styled from "styled-components";
 // Importamos la conexion de la bd para poder realizar operaciones
 import db from "../firebase/firebaseConfig";
-import { useState } from "react";
-import Contacto from "./Contacto";
+// collection permite ingresar a una coleccion y onsnapshot devulve los valores actualizados
+import { collection, onSnapshot } from "firebase/firestore";
 
 const ListaContactos = () => {
 
     {/* Creamos el  estado el cual guardara los usuarios registrados con useState*/}
+    const [usuarios, setUsuarios] = useState([]);
 
-    const [usuarios, setUsuarios] = useState([
-        {
-            id: 1,
-            nombre: 'Alejandro',
-            correo: 'correo@correo.com'
-        },
+    {/* useEffect permite cargar la informacion cuando se inicialice el componente*/}
+    useEffect(() => {
+        {/* Recibe 2 argumentos 
+            1.- colleccion a la que va leer datos
+            2.- Funcion que se ejecuta cada que se realiza un cambio
+        */}
+        onSnapshot(
+            collection(db, 'usuarios'), 
+            (snapshot) => {
+                // console.log(snapshot.docs[0].data());
 
-        {
-            id: 2,
-            nombre: 'Alex',
-            correo: 'correo2@correo.com'
-        }
-    ]);
+                {/* Se recorre cada  usuario y se agrega al arreglo */}
+                const arregloUsuarios = snapshot.docs.map((documento) => {
+                    return {
+                        id: documento.id,
+                        ...documento.data()
+                    }
+                })
+
+                {/* Agregamos el arreglo de usuarios a nuestro estado */}
+                setUsuarios( arregloUsuarios );
+                
+            }
+        )
+    },[])
 
     return(
         //Validamos si el usuario tiene contenido
